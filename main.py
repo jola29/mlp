b3431abd9da845b3af6bc1cb2c43833dce347a40
jola29 <56739466+jola29@users.noreply.github.com>
2023-07-18 16:57:15 +0200

    testlauf gestartet, echt schlechte accuracy xd

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from generate_data import *
 
 
 
-x_train = random_vectors(10)
+x_train = random_vectors(1000)
 y_train = torch.tensor([test_if_in_circle(vect) for vect in x_train],requires_grad=True)
 
 
@@ -49,14 +49,16 @@ print("Finished Training")
 
 correct = 0
 total = 0
-'''
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+
+with torch.no_grad(): #stops doing gradient computation, i.e. training of the model
+    x_test = random_vectors(100)
+    y_test = torch.tensor([test_if_in_circle(vect) for vect in x_test],requires_grad=True)
+
+    outputs = model(x_test)
+    _, pred = torch.max(outputs.data, 1)#gives index of maximum value
+    predicted = torch.tensor([[(1.0+i)%2 ,(0.0+i)%2] for i in pred])#use those indices to create a vecor similar to those in y_test 
+    total += y_test.size(0)
+    correct += (predicted == y_test).sum().item()
 
 print(f"Accuracy: {100 * correct / total}%")
-'''
+
