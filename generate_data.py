eea7866c2c1364fbdddd8d0f0026f64a149c733e
jola29 <56739466+jola29@users.noreply.github.com>
2023-07-18 16:40:28 +0200

    output von test_if_in_circle auf floats geändert (wichtig für .backward funktion...)

--- generate_data.py
+++ generate_data.py
@@ -9,9 +9,18 @@ def random_vectors(n):
     '''
     returns a pytorch tensor with n (uniformly dirstributed) random vectors (x,y) where 0 <= x,y < 1, those vectors are also tensors
     '''
-    return torch.rand(n,2)
+    return torch.rand(n,2,requires_grad=True)
 
-
+def test_if_in_circle(tensor):
+    '''
+    takes a 2D tensor as input. If the coordinates are in the unit circle, returns [1,0], if they are not it returns [0,1]
+    '''
+    if tensor[0]**2 + tensor[1]**2 < 1:
+        return [1.0,0]#we need floats for requires_grad=True in main
+    return [0,1.0]
+    
 
 if __name__=='__main__':
-    print(random_vectors(4))
+    #print(random_vectors(4))
+    x = torch.tensor([0.5,0])
+    print(test_if_in_circle(x))
