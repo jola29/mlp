74afa45cb840424da7354b4895a295dbbe9a71fb
jola29 <56739466+jola29@users.noreply.github.com>
2023-07-18 16:24:55 +0200

    simple model with forward function, should work

--- mlp_model.py
+++ mlp_model.py
@@ -1,17 +1,17 @@
 #build a simple mlp with 2 neurons in the activation layer, 1 hidden layer and 2 nodes in the output layer
 
 #see https://pytorch.org/tutorials/beginner/basics/buildmodel_tutorial.html 
-#for a tutorial on using pytorch to build a neural network
+#and https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 
 import torch
 from torch import nn
+from torch import optim
 from generate_data import *
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(2, 1),
             nn.ReLU(),
@@ -21,20 +21,19 @@ class NeuralNetwork(nn.Module):
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = torch.flatten(x, start_dim=1)
+        x = self.linear_relu_stack(x)
+        return x
 
 if __name__ == '__main__':
+    
     model = NeuralNetwork()
-    print(model)
-
-    X = random_vectors(10)
+    #print(model)
+    X = random_vectors(5)
+    Y = torch.tensor([test_if_in_circle(vect) for vect in X])
+    print(X)
+    print(Y)
 
     
-    #X = torch.rand(1, 28, 28, device=device)
-    logits = model(X)
-    pred_probab = nn.Softmax(dim=1)(logits)
-    y_pred = pred_probab.argmax(1)
-    print(f"Predicted class: {y_pred}")
-    
+
+   
